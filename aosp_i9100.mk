0ea5ce67c347ff85f86913050dd1694c4c8268e0
Christian Oder <thchod@gmail.com>
2014-10-11 16:18:45 +0200

    Fix adaption DERP

--- aosp_i9100.mk
+++ aosp_i9100.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/lge/hammerhead/full_i9100.mk)
+$(call inherit-product, device/samsung/i9100/full_i9100.mk)
 
 PRODUCT_NAME := aosp_i9100
 
